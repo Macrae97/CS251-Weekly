c3ddbeefd97ef41dfa4f74677a1490b570bfc75a
Macrae97 <macrae11@live.com>
2019-10-09 20:07:35 +0100

    more work on week 3

--- CS251-Weekly-Exercises/src/week3/GradeStatistics.java
+++ CS251-Weekly-Exercises/src/week3/GradeStatistics.java
@@ -8,14 +8,13 @@ public class GradeStatistics {
 
         Parser parser = new Parser();
 
-        int[] grades;
+        int[] studentGrades;
         int students = 0;
         boolean finished = false;
 
         while (!finished) {
 
             System.out.println("Enter the number of students in the class.");
-
             try {
                 //prompt user for input
                 students = parser.getInput();
@@ -23,22 +22,53 @@ public class GradeStatistics {
                 System.err.println("Value entered is not valid");
             }
 
-            grades = new int[students];
+            studentGrades = new int[students];
+            studentGrades = getStudentGrades(parser, studentGrades, students);
+            System.out.println("The grades are " + Arrays.toString(studentGrades));
 
-            if (students > 0) {
-                for (int i = 0; i < students; i++) {
-                    int grade = -1;
-                    while(grade < 0 || grade > 100){
-                        System.out.println("Enter the grade for student " + (i+1) + " (0-100");
-                        grade = parser.getInput();
-                    }
-                    grades[i] = grade;
-                }
-                System.out.println("The values are " + Arrays.toString(grades));
-                finished = true;
-            } else {
-                System.out.println("Number of students must be more than 0");
-            }
+            Arrays.sort(studentGrades);
+            System.out.println("The average is: " + Arrays.stream(studentGrades).average().orElse(Double.NaN));
+            System.out.println("The median is: " + getMedian(studentGrades));
+            System.out.println("The minimum is: " + studentGrades[0]);
+            System.out.println("The maximum is: " + studentGrades[studentGrades.length-1]);
+            System.out.println("The standard deviation is: ");
+
+            finished = true;
         }
     }
+
+    private int[] getStudentGrades(Parser parser, int[] studentGrades, int students) {
+        if (students > 0) {
+            for (int i = 0; i < students; i++) {
+                int grade = -1;
+                while(grade < 0 || grade > 100){
+                    System.out.println("Enter the grade for student " + (i+1) + " (0-100");
+                    grade = parser.getInput();
+                }
+                studentGrades[i] = grade;
+            }
+        } else {
+            System.out.println("Number of students must be more than 0");
+        }
+
+        return studentGrades;
+    }
+
+    //code found at https://codippa.com/how-to-calculate-median-from-array-values-in-java/
+    private double getMedian(int[] grades) {
+        double median;
+        // get count of scores
+        int totalElements = grades.length;
+        // check if total number of scores is even
+        if (totalElements % 2 == 0) {
+            int sumOfMiddleElements = grades[totalElements / 2] + grades[totalElements / 2 - 1];
+            // calculate average of middle elements
+            median = ((double) sumOfMiddleElements) / 2;
+        } else {
+            // get the middle element
+            median = (double) grades[grades.length / 2];
+        }
+        return median;
+    }
+
 }
